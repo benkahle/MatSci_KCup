756d5dccb41b17c50bc54509fbfb056841ad708d
Jamie Gorson <jgorson@ubuntu.(none)>
2014-09-24 16:38:44 -0400

    make comparison graph

--- tga_analysis.py
+++ tga_analysis.py
@@ -3,9 +3,18 @@ import pandas
 import math
 import sys
 
+maxWeightFilterHold = 19.64691
+maxWeightCasingHold = 5.15985
 
-print sys.argv[1]
-dataframe = pandas.read_table(sys.argv[1], sep=',', names=['Time', 'Temp', 'Weight'])
-pp.plot(dataframe['Time'],dataframe['Weight'])
+
+dFCasingHold = pandas.read_table('casing_hold.csv', sep=',', names=['Time', 'Temp', 'Weight'])
+dFCasingSlowRamp = pandas.read_table('casing_slowRamp.csv', sep=',', names=['Time', 'Temp', 'Weight'])
+dFFilterHold =pandas.read_table('plastic_filter_hold.csv', sep=',', names=['Time', 'Temp', 'Weight'])
+dfFilterSlowRamp = pandas.read_table('plastic_filter_slowRamp.csv', sep=',', names=['Time', 'Temp', 'Weight'])
+
+dFFilterHold['Weight'] = dFFilterHold['Weight']/maxWeightFilterHold
+dFCasingHold['Weight'] = dFCasingHold['Weight']/maxWeightCasingHold
+pp.plot(dFCasingHold['Time'][940:],dFCasingHold['Weight'][940:], 'r')
+pp.plot(dFFilterHold['Time'][367:],dFFilterHold['Weight'][367:])
 pp.show()
 print(dataframe)
