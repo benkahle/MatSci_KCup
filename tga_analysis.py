ff3d0626cc52b4d9c4f91ede5f69b843162ff850
Jamie Gorson <jgorson@ubuntu.(none)>
2014-09-24 15:25:26 -0400

    plot initial graph

--- tga_analysis.py
+++ tga_analysis.py
@@ -2,6 +2,10 @@ import matplotlib.pyplot as pp
 import pandas
 import math
 import sys
+
+
 print sys.argv[1]
-dataframe = pandas.read_table(sys.argv[1], sep=',')
+dataframe = pandas.read_table(sys.argv[1], sep=',', names=['Time', 'Temp', 'Weight'])
+pp.plot(dataframe['Time'],dataframe['Weight'])
+pp.show()
 print(dataframe)
