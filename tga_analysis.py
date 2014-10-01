8a41dc02ff3be3ea70888ba0c9cf8ec8ff589384
Jamie Gorson <jgorson@ubuntu.(none)>
2014-10-01 16:57:24 -0400

    finilized graphs

--- tga_analysis.py
+++ tga_analysis.py
@@ -6,7 +6,9 @@ import sys
 maxWeightFilterHold = 19.64691
 maxWeightCasingHold = 5.15985
 maxWeightCasingHold = 9.727528
-maxWeightEkoHold = 37.15359
+maxWeightEkoHold = 37.19263  #37.15359
+# maxWeightEkoHold = 45.21924
+
 
 
 dFCasingHold = pandas.read_table('casing_hold2.csv', sep=',', names=['Time', 'Temp', 'Weight'])
@@ -24,14 +26,24 @@ dFCasingHold['Weight'] = dFCasingHold['Weight']/maxWeightCasingHold
 dfEkoHold['Weight'] = dfEkoHold['Weight']/maxWeightEkoHold
 
 
-pp.plot(dFCasingHold['Time'][0:500],dFCasingHold['Weight'][0:500], 'r')
-#pp.plot(dFFilterHold['Time'][367:],dFFilterHold['Weight'][367:])
-pp.plot(dfEkoHold['Time'][0:500],dfEkoHold['Weight'][0:500])
 
-pp.plot(dFCasingHold['Time'],dFCasingHold['Weight'], 'r')
-pp.plot(dfEkoHold['Time'],dfEkoHold['Weight'])
+pp.plot(dFCasingHold['Time'],dFCasingHold['Weight']*100, 'r')
+pp.plot(dfEkoHold['Time'],dfEkoHold['Weight']*100, 'g')
 pp.title('Mass lost while holding at 89 degrees Celcius')
 pp.legend(['Casing','EkoCup'])
 pp.xlabel('Time (minutes)')
-pp.ylabel('Mass')
+pp.ylabel('Percent Mass')
+pp.show()
+
+
+pp.figure()
+pp.clf()
+pp.plot(dFCasingHold['Time'][0:300],dFCasingHold['Weight'][0:300]*100, 'r')
+#pp.plot(dFFilterHold['Time'][367:],dFFilterHold['Weight'][367:])
+pp.plot(dfEkoHold['Time'][0:300],dfEkoHold['Weight'][0:300]*100, 'g')
+
+pp.title('Mass lost while holding at 89 degrees Celcius')
+pp.legend(['Casing','EkoCup'])
+pp.xlabel('Time (minutes)')
+pp.ylabel('Percent Mass')
 pp.show()
